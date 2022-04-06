5bcf858670cf6104f1bb2e2bcc929cdfb15395ef
Max Kryzhanovskiy <kryzhanovskiymax@mail.ru>
2022-04-06 22:41:04 +0300

    some changes in GraphPainter

--- graph_algo/graph_painter.hpp
+++ graph_algo/graph_painter.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <unordered_map>
+#include <vector>
+
 #include "svg/svg.hpp"
 
 namespace graph {
@@ -12,13 +15,13 @@ namespace domain {
     };  
 
     struct GraphRepresentationConfig {
-        int stroke_width;
-        int vertice_radius;
-        std::vector<svg::Color> palette;
-        std::vector<svg::Color> vertice_palette;
-        std::vector<svg::Color> edge_palette;
-        svg::Color mst_color;
-        int mst_line_width;
+        int stroke_width = 1;
+        int vertice_radius = 3;
+        std::vector<svg::Color> palette = {svg::Rgb(45, 45, 60)};
+        std::vector<svg::Color> vertice_palette = {svg::Rgb(60, 80, 80)};
+        std::vector<svg::Color> edge_palette = {svg::Rgb(100, 100, 100)};
+        svg::Color mst_color = svg::Rgb(150, 10, 10);
+        int mst_line_width = 2;
     };
 
     struct RepresentationFlags {
@@ -31,10 +34,32 @@ namespace domain {
 class GraphPainter {
 public:
 
+    GraphPainter() {};
+    GraphPainter(Size size, GraphRepresentationConfig config, RepresentationFlags flags):
+                    image_size(size), representation_config(config), representation_flags(flags) {}
+    
+    GraphPainter& SetStrokeWidth(int width) {
+        representation_config.stroke_width = width;
+        return *this;
+    }
+
+    GraphPainter& SetVerticeRadius(int radius) {
+        representation_config.vertice_radius = radius;
+        return *this;
+    }
+
+    GraphPainter& SetMSTLineWidth(int width) {
+        representation_config.mst_line_width = width;
+        return *this;
+    }
+
 private:
     domain::Size image_size;
     domain::GraphRepresentationConfig representation_config;
-    domain::RepresentationFlag representation_flag;
+    domain::RepresentationFlag representation_flags;
+    std::unordered_map<std::string, svg::Point> vertices_to_positions;
+
+
 };
 
 }
